441e66060c9fd52c346f6b3f93625eeb907cbeb2
hengmolika <molika.heng@student.passerellesnumeriques.org>
2021-11-28 15:20:34 +0700

    #7 manage user for backed

--- back/database/migrations/2021_11_28_073316_create_joins_table.php
+++ back/database/migrations/2021_11_28_073316_create_joins_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateJoinsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,10 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('joins', function (Blueprint $table) {
             $table->id();
-            $table->string('first_name');
-            $table->string('last_name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->foreignId("event_id")->constrained()->onDelete("CASCADE");
+            $table->foreignId("user_id")->constrained()->onDelete("CASCADE");
             $table->timestamps();
         });
     }
@@ -32,6 +28,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('joins');
     }
 }
--- back/routes/api.php
+++ back/routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\UserController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,6 +14,23 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+//-------------User Public Route ---------------------------
+Route::post('register', [UserController::class, 'register']);
+Route::post('login', [UserController::class, 'login']);
+
+Route::get('users', [UserController::class, 'index']);
+Route::get('users/{id}', [UserController::class, 'show']);
+Route::delete('users/{id}', [UserController::class, 'destroy']);
+
+
+
+//-------------- Private Route ------------------------------
+// Route::group(['middleware' => ['auth:sanctum']], function() {
+//     Route::post('events', [PostController::class, 'store']);
+//     Route::put('events/{id}', [PostController::class, 'update']);
+//     Route::delete('events/{id}', [PostController::class, 'destroy']);
+
+//     Route::post('logout', [UserController::class, 'logout']);
+// });
+
+Route::post('logout', [UserController::class, 'logout']);
