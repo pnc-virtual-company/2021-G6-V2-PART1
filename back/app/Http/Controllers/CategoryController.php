63c23d61631a96d85344aab7828b5002027e46db
sopheakvon <sopheak.von@student.passerellesnumeriques.org>
2021-12-02 11:11:35 +0700

    #15 Manage Categories on backend

---
+++ back/app/Http/Controllers/CategoryController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Category;
+class CategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Category::latest()->get();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            "name" => "required|unique:categories"
+        ]);
+        $category = new Category();
+        $category->name = $request->name;
+        $category->save();
+        return response()->json(["message" => "Category created", 'data' => $category],201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return Category::findOrFail($id);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+       
+        $category = Category::findOrFail($id);
+        $category->name = $request->name;
+        $category->save();
+        return response()->json(["message" => "Category Updated"],200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        return Category::destroy($id);
+    }
+}
--- back/routes/api.php
+++ back/routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\CategoryController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,3 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/category',[CategoryController::class, "index"]);
+Route::get('/category/{id}',[CategoryController::class, "show"]);
+Route::post('/category',[CategoryController::class, "store"]);
+Route::put('/category/{id}',[CategoryController::class, "update"]);
+Route::delete('/category/{id}',[CategoryController::class, "destroy"]);
