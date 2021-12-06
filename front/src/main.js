9f30e48d8d3cedddb77e578c0a3151a550395b76
hengmolika <molika.heng@student.passerellesnumeriques.org>
2021-12-06 08:51:49 +0700

    #23 myeventcard

--- front/src/main.js
+++ front/src/main.js
@@ -1,4 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-
-createApp(App).mount('#app')
+import MyEventCard from './components/myevent/MyEventCard.vue';
+const app = createApp(App);
+app.component('my-event-card', MyEventCard);
+app.mount('#app');
