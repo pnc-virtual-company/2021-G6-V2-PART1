4c7f2a1b04969159e5ca1732c1bd15d3b5a33f15
Koemsak <koemsak.mean@student.passerellesnumeriques.org>
2021-12-07 18:55:39 +0700

    Make change

---
+++ front/src/components/Pages/MyEvent/MyEventView.vue
@@ -0,0 +1,33 @@
+<template>
+  <section>
+    <div class="container p-4">
+        <div class="d-flex justify-content-between">
+            <h3 class="text-primary">My Event</h3>
+        </div>
+        <hr class="bg-primary">
+        <form class="form">
+            <div class="input-group">
+            <input
+                type="text"
+                class="form-control"
+                placeholder="Username"
+                aria-label="Username"
+                aria-describedby="basic-addon1"
+            />
+            <div class="input-group-prepend">
+                <button class="fa fa-search btn btn-primary rounded-right"></button>
+            </div>
+            </div>
+        </form>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+};
+</script>
+
+<style>
+
+</style>
--- front/src/main.js
+++ front/src/main.js
@@ -1,4 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import MyEventView from './components/Pages/MyEvent/MyEventView.vue'
 
-createApp(App).mount('#app')
+
+
+const app = createApp(App);
+app.component('my-event-view', MyEventView)
+
+app.mount('#app')
